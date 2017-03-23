a2f4f4221e6255517be93b52b14cde269f4de8ab
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-03-22 21:50:34 -0300

    Remove unused code
    For now, neither algorithm is under development. So the unused code
    has been removed

--- include/jvanz/graph.hpp
+++ include/jvanz/graph.hpp
@@ -1,19 +1,8 @@
 #ifndef __JVANZ_GRAPH_HPP__
 #define __JVANZ_GRAPH_HPP__
 
-#include <vector>
-#include <tuple>
-#include <future>
-
-using namespace std;
-
 namespace jvanz {
 namespace graph {
-
-typedef tuple<unsigned int,unsigned int, int> edge_tuple;
-
-future<int> find_shortest_path_all_pairs();
-
 } // namespace graph
 } // namespace jvanz
 
