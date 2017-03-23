2e872bdc14e10c02a8950f41651180c175270877
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-03-23 00:06:43 -0300

    algorithm_design.cpp: initial floyd-warshall code
    An initial and not finished floyd-warshall algorithm implementation has
    been added in the algorithm_design.cpp

--- tests/algorithm_design.cpp
+++ tests/algorithm_design.cpp
@@ -17,6 +17,7 @@ static Graph ReadAssignment4Data(const string file_path)
 		for (auto i = 0; i < edges; ++i){
 			unsigned int source; unsigned int dest; int cost;
 			file >> source >> dest >> cost;
+			g[source-1][dest-1] = cost;
 			if (file.bad())
 				throw "Error while reading input file";
 		}
@@ -25,29 +26,38 @@ static Graph ReadAssignment4Data(const string file_path)
 	throw "Can not read input file";
 }
 
-static bool validate_values(const Graph& g)
+static void floyd_warshall(Graph& graph)
 {
-	for (unsigned int i = 0; i < g.size(); ++i){
-		for (unsigned int ii = 0; ii < g[i].size(); ++ii){
-			if (g[i][ii] != numeric_limits<int>::max())
-				return false;
+	auto vertices = graph.size();
+	// initialize default values of the diagonal
+	for (auto i = 0; i < vertices; ++i)
+		graph[i][i] = 0;
+	for (auto k = 0; k < vertices; ++k){
+		for (auto i = 0; i < vertices; ++i){
+			for (auto j = 0; j < vertices; ++j){
+				if (graph[i][j] > graph[i][k] + graph[k][j])
+					graph[i][j] = graph[i][k] + graph[k][j];
+			}
 		}
 	}
-	return true;
 }
 
 TEST(Assignment4, Algorith2MOOCCoursera) {
 	auto graph = ReadAssignment4Data("data/g1.txt");
 	ASSERT_EQ(graph.size(),1000);
 	ASSERT_EQ(graph[0].size(),1000);
-	ASSERT_TRUE(validate_values(graph));
+	ASSERT_EQ(graph[0][13],6);
+	ASSERT_EQ(graph[999][987],3);
+	floyd_warshall(graph);
 	graph = ReadAssignment4Data("data/g2.txt");
 	ASSERT_EQ(graph.size(),1000);
 	ASSERT_EQ(graph[0].size(),1000);
-	ASSERT_TRUE(validate_values(graph));
+	ASSERT_EQ(graph[0][1],2);
+	ASSERT_EQ(graph[999][987],3);
 	graph = ReadAssignment4Data("data/g3.txt");
 	ASSERT_EQ(graph.size(),1000);
 	ASSERT_EQ(graph[0].size(),1000);
-	ASSERT_TRUE(validate_values(graph));
+	ASSERT_EQ(graph[0][7],36);
+	ASSERT_EQ(graph[999][978],1);
 }
 
