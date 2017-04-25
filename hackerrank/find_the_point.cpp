f9c94ea01afeaf70e129d596ea3258c7bd93a4fa
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-04-25 00:16:31 -0300

    find_the_point.cpp: hacker rank math challenges
    Adding the initial code to solve a Hacker Rank math challenge to find
    the point reflection. https://www.hackerrank.com/challenges/find-point

---
+++ hackerrank/find_the_point.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool is_point_valid(int p)
+{
+	return -100 <= p <= 100;
+}
+
+int main(int argc, char** argv)
+{
+	// read input
+	unsigned int n;
+	cin >> n;
+	if (n < 1 && n > 15)
+		return -1;
+	while (n--) {
+		int px, py, qx, qy;
+		cin >> px >> py >> qx >> qy;
+		if (!is_point_valid(px) && !is_point_valid(py)
+			&& !is_point_valid(qx) && !is_point_valid(qy)){
+			return -1;
+		}
+		// TODO set the values in some useful data structure
+	}
+	// TODO find point reflection
+}
+
+
