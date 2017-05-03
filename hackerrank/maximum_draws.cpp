8786c9663c40e15d67c63105fa1257af984426a4
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-05-02 23:27:44 -0300

    maximum_draws: hacker rank challenge

---
+++ hackerrank/maximum_draws.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	unsigned int t;
+	cin >> t;
+	if (t < 1 && t > 1000)
+		return -1;
+	while (t--) {
+		unsigned int n;
+		cin >> n;
+		if (n > pow(10,6))
+			return -1;
+		cout << n + 1 << endl;
+	}
+}
+
