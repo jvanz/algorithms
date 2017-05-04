35c80b229068aad8e8c420a0b044333341187693
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-05-03 23:42:35 -0300

    handshake.cpp: handshake challenge from hacker rank
    Added initial code to solve the handleshake challenge from math
    track in hackerrank.com

---
+++ hackerrank/handshake.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	int T;
+	cin >> T;
+	if (T < 1 || T > 1000){
+		cout << "Invalid T" << endl;
+		return -1;
+	}
+	while (T--){
+		int N;
+		cin >> N;
+		if (N <= 0 || N >= pow(10,6)){
+			cout << "Invalid N" << endl;
+			return -1;
+		}
+		cout << N << endl;
+	}
+	return 0;
+}
