4ea8c4fa6e46e35e1c65f9b09635f6d4b1a15839
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-06-14 21:45:37 -0300

    hackerrank: dynamic array challenge
    This commit adds the solution for the dynamic array challenge of the
    data structure track in Hacker Rank web site.

--- hackerrank/data_structures/dynamic_array.cpp
+++ hackerrank/data_structures/dynamic_array.cpp
@@ -7,6 +7,26 @@
 using namespace std;
 
 int main() {
-	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	unsigned int N, Q;
+	cin >> N >> Q;
+	int lastAnswer = 0;
+	vector<vector<int>> seqList(N);
+	while (Q--) {
+		unsigned int type;
+		cin >> type;
+		if (type == 1 || type == 2) {
+			unsigned int x, y;
+			cin >> x >> y;
+			auto index = (x ^ lastAnswer) % N;
+			if (type == 1) {
+				seqList[index].push_back(y);
+			} else {
+				lastAnswer = seqList[index][y % seqList[index].size()];
+				cout << lastAnswer << endl;
+			}
+		} else {
+			return -1;
+		}
+	}
 	return 0;
 }
