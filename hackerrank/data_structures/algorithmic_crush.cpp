5562a8d2285d90cfd95222eb8494617f192546cf
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-06-20 23:33:05 -0300

    algorithmic_crush: final solution
    Added the final solution for the algorithmic crush challenge. This
    solution is from the discussion forums.

--- hackerrank/data_structures/algorithmic_crush.cpp
+++ hackerrank/data_structures/algorithmic_crush.cpp
@@ -9,17 +9,26 @@ using namespace std;
 int main() {
 	unsigned int N, M;
 	cin >> N >> M;
-	vector<unsigned int> arr(N);
-	unsigned int max = 0;
+	if ((N < 3 || N > pow(10, 7)) || M > (2 * pow(10,5)))
+		return -1;
+
+	vector<long> arr(N);
+	long max = 0;
 	while (M--){
 		unsigned int a, b, k;
 		cin >> a >> b >> k;
-		a--; b--; // array is 0 index
-		for (; a <= b; a++){
-			arr[a] += k;
-			if (arr[a] > max)
-				max = arr[a];
-		}
+		if ( a > b || a > N || b > N)
+			return -1;
+		a--; b--; //  array is 0 index
+		arr[a] += k;
+		if ((b+1) <= N)
+			arr[b+1] -= k;
+	}
+	long x = 0;
+	for (auto i = 0; i < N; i++){
+		x = x + arr[i];
+		if (max < x)
+			max = x;
 	}
 	cout << max << endl;
 }
