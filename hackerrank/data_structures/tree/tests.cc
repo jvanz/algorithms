1f053aa2d8a5cd04975fb6d12e48288c67649867
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-14 00:47:28 -0200

    tests: post/in order test
    Adds a test case to validate the recursive post/in order function
    implementation
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- hackerrank/data_structures/tree/tests.cc
+++ hackerrank/data_structures/tree/tests.cc
@@ -62,69 +62,65 @@ TEST(Tree, pre_order_iterator)
 	ASSERT_EQ(9, x);
 }
 
-//TEST(Tree, PrintPreOrder)
-//{
-	//auto zero = new struct node();
-	//auto one = new struct node();
-	//auto two = new struct node();
-	//auto three = new struct node();
-	//auto four = new struct node();
-	//zero->data = 0;
-	//one->data = 1;
-	//two->data = 2;
-	//three->data = 3;
-	//four->data = 4;
-	//one->left = zero;
-	//one->left = two;
-	//two->left = three;
-	//two->right = four;
-	//PrintPreOrder(one);
+TEST(Tree, post_order_iterator)
+{
+	auto zero = new TreeNode(0);
+	auto one = new TreeNode(1);
+	auto two = new TreeNode(2);
+	auto three = new TreeNode(3);
+	auto four = new TreeNode(4);
+	auto five = new TreeNode(5);
+	auto six = new TreeNode(6);
+	auto seven = new TreeNode(7);
+	auto eight = new TreeNode(8);
+	zero->left = one;
+	one->left = two;
+	one->right = three;
+	zero->right = four;
+	four->left = five;
+	four->right = six;
+	six->left = seven;
+	six->right = eight;
+	unsigned int order[9] = {2, 3, 1, 5, 7, 8, 6, 4, 0};
+	auto x = 0;
+	post_order_iterator(zero, 
+		[&x, &order] (int data) {
+			ASSERT_EQ(order[x], data);
+			x++;
+		}
+	);
+	ASSERT_EQ(9, x);
+}
 
-//}
-
-//TEST(Tree, PrintPostOrder)
-//{
-	//auto one = new struct node();
-	//auto two = new struct node();
-	//auto three = new struct node();
-	//auto four = new struct node();
-	//auto five = new struct node();
-	//auto six = new struct node();
-	//one->data = 1;
-	//two->data = 2;
-	//three->data = 3;
-	//four->data = 4;
-	//five->data = 5;
-	//six->data = 6;
-	//one->right = two;
-	//two->right = five;
-	//three->right = four;
-	//five->left = three;
-	//five->right = six;
-	//PrintPostOrder(one);
-//}
-
-//TEST(Tree, PrintInOrder)
-//{
-	//auto one = new struct node();
-	//auto two = new struct node();
-	//auto three = new struct node();
-	//auto four = new struct node();
-	//auto five = new struct node();
-	//auto six = new struct node();
-	//one->data = 1;
-	//two->data = 2;
-	//three->data = 3;
-	//four->data = 4;
-	//five->data = 5;
-	//six->data = 6;
-	//one->right = two;
-	//two->right = five;
-	//five->left = three;
-	//five->right = six;
-	//three->right = four;
-	//PrintInOrder(one);
-//}
+TEST(Tree, in_order_iterator)
+{
+	auto zero = new TreeNode(0);
+	auto one = new TreeNode(1);
+	auto two = new TreeNode(2);
+	auto three = new TreeNode(3);
+	auto four = new TreeNode(4);
+	auto five = new TreeNode(5);
+	auto six = new TreeNode(6);
+	auto seven = new TreeNode(7);
+	auto eight = new TreeNode(8);
+	zero->left = one;
+	one->left = two;
+	one->right = three;
+	zero->right = four;
+	four->left = five;
+	four->right = six;
+	six->left = seven;
+	six->right = eight;
+	unsigned int order[9] = {2, 1, 3, 0, 5, 4, 7, 6, 8};
+	auto x = 0;
+	in_order_iterator(zero, 
+		[&x, &order] (int data) {
+			ASSERT_EQ(order[x], data);
+			x++;
+		}
+	);
+	ASSERT_EQ(9, x);
+}
 
 //TEST(Tree, GetHeight)
 //{
