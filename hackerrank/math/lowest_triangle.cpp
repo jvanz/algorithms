aaea1e410ac6e17c3054dfa0f164281b46062be2
José Guilherme Vanz <guilherme.sft@gmail.com>
2018-06-12 23:32:41 -0300

    lowest_triangle.cpp: find smallest height
    Given integers b and a, find the smallest integer h, such that there
    exists a triangle of height h, base b, having an area of at least a.
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

---
+++ hackerrank/math/lowest_triangle.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int lowestTriangle(int base, int area){
+	return ceil((float)(2 * area) / base);
+}
+
+int main()
+{
+	int base;
+	int area;
+	cin >> base >> area;
+	int height = lowestTriangle(base, area);
+	cout << height << endl;
+	return 0;
+}
