a04b95f051bf45158e71babaa04649e80c04a0d4
José Guilherme Vanz <guilherme.sft@gmail.com>
2018-06-13 23:13:44 -0300

    libmath: add a lib with math functions
    Add a math library with functions created during the HackerRank
    challenges
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

---
+++ hackerrank/math/math.cpp
@@ -0,0 +1,7 @@
+
+#include <math.hpp>
+
+int jvanz::math::prime_count(long n)
+{
+	return 0;
+}
---
+++ hackerrank/math/math.hpp
@@ -0,0 +1,13 @@
+
+
+
+namespace jvanz {
+namespace math {
+
+/**
+ * https:// www.hackerrank.com/challenges/leonardo-and-prime/problem
+ */
+int prime_count(long n);
+
+}
+}
